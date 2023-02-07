3c8ee46c521bc2bf6f55421e9915fbdb3afa10ef
pinalh3 <pinalh3@gmail.com>
2023-02-07 16:05:36 -0400

    Ejercicio3 sesion 4 5 6

--- src/main/java/com/example/obspringbootses456/controller/LaptopController.java
+++ src/main/java/com/example/obspringbootses456/controller/LaptopController.java
@@ -3,6 +3,8 @@ package com.example.obspringbootses456.controller;
 import com.example.obspringbootses456.entity.Laptop;
 import com.example.obspringbootses456.repository.LaptopRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +20,10 @@ public class LaptopController {
     @GetMapping("/api/laptop")
     public List<Laptop> findAll(){
         return laptopRepository.findAll();
+    }
+    @PostMapping("/api/laptop")
+    public Laptop create(@RequestBody Laptop laptop){
+        return laptopRepository.save(laptop);
     }
 
 
