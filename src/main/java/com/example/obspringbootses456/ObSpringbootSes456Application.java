fcc8783f6fec572dc342920c5c5f7b90d62dff1d
pinalh3 <pinalh3@gmail.com>
2023-02-07 15:02:22 -0400

    Ejercicio1 sesion 4 5 6

--- src/main/java/com/example/obspringbootses456/ObSpringbootSes456Application.java
+++ src/main/java/com/example/obspringbootses456/ObSpringbootSes456Application.java
@@ -1,13 +1,17 @@
 package com.example.obspringbootses456;
 
+import org.aspectj.apache.bcel.Repository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ObSpringbootSes456Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ObSpringbootSes456Application.class, args);
+
+		ApplicationContext context = SpringApplication.run(ObSpringbootSes456Application.class, args);
+
 	}
 
 }
--- src/main/java/com/example/obspringbootses456/controller/HelloController.java
+++ src/main/java/com/example/obspringbootses456/controller/HelloController.java
@@ -1,4 +1,12 @@
 package com.example.obspringbootses456.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/")
+    public String hola(){
+        return "Saludos desde Venezuela";
+    }
 }
